f0b3455c30b3c949acfb1f97bc1e9f43717705ec
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-17 10:14:19 +0530

    analysis on the data.

---
+++ src/analysis.py
@@ -0,0 +1,43 @@
+def average_salary(df):
+    print("\n Average Salary")
+    print(df["Salary"].mean())
+
+def highest_salary(df):
+    print("\n highest Salary")
+    print(df["Salary"].max())
+
+def lowest_salary(df):
+    print("\n lowest Salary")
+    print(df["Salary"].min())
+
+def department_salary(df):
+    print("\n Average salary by department")
+    print(df.groupby("Department")["salary"].mean())
+
+def city_salary(df):
+    print("\n Average salary by city")
+    print(df.groupby("City")["salary"].mean())
+
+def gender_salary(df):
+    print("\n Average salary by gender")
+    print(df.groupby("Gender")["salary"].mean())
+
+def education_salary(df):
+    print("\n Average salary by education")
+    print(df.groupby("Education")["salary"].mean())
+
+def performance_count(df):
+    print("\n Performance count")
+    print(df["Performance"].value_count())
+
+def work_mode_count(df):
+    print("\n work mode count")
+    print(df["Work Mode"].value_count())
+
+def top_5_salary(df):
+    print("\nTop 5 Salary")
+    print(df.nlargest(5,"Salary"))
+
+def bottom_5_salary(df):
+    print("\nBottom 5 lowest paid Employees")
+    print(df.nsmallest(5, "Salary"))
