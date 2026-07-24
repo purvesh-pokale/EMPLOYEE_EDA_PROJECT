04b4be71839c2be6f0774c53306beb0a413c253d
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-24 10:19:29 +0530

    creating the main file

---
+++ main.py
@@ -0,0 +1,62 @@
+ #import Modules
+from src.load_data import load_data
+from src.preprocessing import *
+from src.analysis import *
+from src.visualization import *
+from src.report import generate_report
+
+
+#create the required floder
+os.makedirs("images",exist_ok=True)
+os.makedirs("report",exist_ok=True)
+
+#lode the dateset
+print("\n"+"="*60)
+print("Lodeing Dataset")
+print("=",*60)
+
+df= load_data("data/employee_data.csv")
+
+
+# Data Preprocessing
+print("\n"+"="* 60)
+print("DATA .PREPROCESSING")
+print("=" *60)
+
+dataset_info(df)
+
+check_missing_values(df)
+
+check_duplicates(df)
+
+df = remove_duplicates(df)
+
+save_clean_data(df)
+
+#Analysis
+print("\n" +"=" * 60)
+print("BUSINESS ANALYSIS")
+print("=" * 60)
+
+average_salary(df)
+
+highest_salary(df)
+
+lowest_salary(df)
+
+department_salary(df)
+
+city_salary(df)
+
+#Genert PDF Report
+
+print("\n" +"=" * 60)
+print("Genert PDF Repor")
+print("=" * 60)
+
+generate_report(df)
+
+#Project Completed
+print("\n" +"=" * 60)
+print("Project Completed successfully")
+print("=" * 60)
