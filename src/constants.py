7dbcc892e2e63524ad0861f82b3aafdae5d598c0
purvesh-pokale <pokalepurvesh2003@gmail.com>
2026-07-27 22:02:48 +0530

    it help when we want to modify If a path or configuration changes

---
+++ src/constants.py
@@ -0,0 +1,13 @@
+# src/constants.py
+
+DATA_PATH = "data/employee_data.csv"
+
+REPORT_FOLDER = "report"
+
+IMAGE_FOLDER = "images"
+
+REPORT_NAME = "Employee_EDA_Report.pdf"
+
+FIGURE_SIZE = (8, 5)
+
+DPI = 300
